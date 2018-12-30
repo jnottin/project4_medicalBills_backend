36c1827bd86929d85a2dfdf0af8c9ed56ca53755
Sam Nottingham <jnottin92@gmail.com>
2018-12-30 11:57:31 -0500

    made single model branch

--- models/hopsital.js
+++ models/hopsital.js
@@ -14,7 +14,16 @@ const Hospital = new Schema({
     lng: Number,
     lat: Number,
     cost: Number,
-    procedures: [Procedure],
+    appendectomy_cost: [Number],
+    breast_biopsy_cost: [Number],
+    carotid_endarterectomy_cost: [Number],
+    cataract_surgery_cost: [Number],
+    cesarean_section_cost: [Number],
+    coronary_artery_bypass_cost: [Number],
+    debridement_of_wound_cost: [Number],
+    free_skin_graft_cost: [Number],
+    spinal_fusion_cost: [Number],
+    total_hip_replacement_cost: [Number],
 });
 
 module.exports = {
