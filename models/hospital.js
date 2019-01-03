8aafa7ddd37cf9a08758d4fd744ff606555fcc44
Sam Nottingham <jnottin92@gmail.com>
2019-01-03 10:39:23 -0500

    commit to heroku

--- models/hospital.js
+++ models/hospital.js
@@ -1,13 +1,6 @@
 const mongoose = require("../db/connection");
 const Schema = mongoose.Schema;
 
-const Procedure = new Schema({
-    name_of_procedure: String,
-    cost: Number,
-    insurance_provider: String,
-    date_of_procedure: String,
-});
-
 const Hospital = new Schema({
     name: String,
     address: String,
