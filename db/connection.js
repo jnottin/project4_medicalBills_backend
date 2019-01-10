39a2d5fdeedc3fbca47a5f34dcc60511f1cc3d29
Sam Nottingham <jnottin92@gmail.com>
2019-01-10 14:59:42 -0500

    seed data

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 if (process.env.NODE_ENV == "production") {
-    mongoose.connect(process.env.MLAB_URL, { useMongoClient: true })
+    mongoose.connect(process.env.MLAB_URL)
 } else {
     mongoose.connect("mongodb://localhost/project4medicalcosts", { useCreateIndex: true, useNewUrlParser: true });
 }
